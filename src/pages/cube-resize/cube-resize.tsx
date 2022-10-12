b01cc5c2ba03a3f819e907d2bd673e1ba88b9b4f
Noah Keller <63370997+proelf98@users.noreply.github.com>
2022-10-12 15:15:36 +0200

    bug type is not assignable

--- src/pages/cube-resize/cube-resize.tsx
+++ src/pages/cube-resize/cube-resize.tsx
@@ -1,43 +1,14 @@
+import * as React from "react";
 import * as THREE from "three";
-import { Form } from "react-bootstrap";
-import { cubeState } from "./cube.state";
-import { useEffect, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import { Box, CubeCamera } from "@react-three/drei";
+import { Vector3 } from "three";
 import { Setup } from "./Setup";
 
-const Range = () => {
-  const cubeSettings = cubeState();
-
-  // Slider Componente hier ergänzen
+const Showcase = () => {
   return (
-    <div>
-      <Form>
-        <Form.Group>
-          <Form.Label>Zoom Value {}</Form.Label>
-          <input
-            type="range"
-            className="form-range"
-            id="customRange1"
-            defaultValue={1}
-            min={cubeSettings.rangeMin}
-            max={cubeSettings.rangeMax}
-            onChange={(event) =>
-              cubeSettings.setCubeHeight(
-                event.target.value as unknown as number
-              )
-            }
-          />
-        </Form.Group>
-      </Form>
-    </div>
-  );
-};
-
-const CubeResize = () => {
-  return (
-    <div style={{ width: "100vw", height: "80vh" }}>
-      <Setup cameraPosition={new THREE.Vector3(0, 10, 40)}>{Scene()}</Setup>
-      {Range()}
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Setup cameraPosition={new Vector3(0, 10, 40)}>{Scene()}</Setup>
     </div>
   );
 };
@@ -50,37 +21,32 @@ declare global {
   }
 }
 
+const sphere = ({ offset = 0, ...props }) => {
+  const ref = React.useRef<THREE.Group>();
+  useFrame(({ clock }) => {
+    ref.current!.position.y = Math.sin(offset + clock.elapsedTime) * 5;
+  });
+
+  return (
+    <CubeCamera {...props}>
+      {(texture) => (
+        <mesh ref={ref}>
+          <sphereGeometry args={[5, 64, 64]} />
+          <meshStandardMaterial roughness={0} metalness={1} envMap={texture} />
+        </mesh>
+      )}
+    </CubeCamera>
+  );
+};
+
 const Scene = () => {
-  const cubeSettings = cubeState();
   return (
     <>
       <fog attach="fog" args={["#f0f0f0", 100, 200]} />
-      <Box cubeHeight={cubeSettings.transformCubeHeight} color={"hotpink"} />
+      <Box material-color="hotpink" args={[5, 5, 5]} position-y={2.5} />
       <gridHelper args={[100, 10]} />
     </>
   );
 };
 
-const Box = ({ cubeHeight = 1, color = "orange", ...restProps }) => {
-  const cubeSettings = cubeState();
-  // This reference will give us direct access to the THREE.Mesh object
-  const ref = useRef<THREE.Mesh>(null!);
-
-  const m = new THREE.Matrix4();
-
-  useEffect(() => {
-    ref.current!.applyMatrix4(
-      m.makeScale(1, cubeSettings.transformCubeHeight, 1)
-    );
-    // ref.current!.position.y = Math.sin(0 + clock.elapsedTime) * 5;
-  });
-
-  return (
-    <mesh ref={ref}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={color} />
-    </mesh>
-  );
-};
-
-export default CubeResize;
+export default Showcase;
